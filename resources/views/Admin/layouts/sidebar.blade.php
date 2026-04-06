59e11c0d25b3f03422ccf4b1280882e002ec545f
Habibur5114 <habiburrahman5114@gmail.com>
2026-04-06 08:43:24 +0600

    update

--- resources/views/Admin/layouts/sidebar.blade.php
+++ resources/views/Admin/layouts/sidebar.blade.php
@@ -390,6 +390,14 @@
                                 </a>
                             </li>
                         </ul>
+                        {{-- <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.slider.index') }}" class="nav-link @yield('sliderList')">
+                                    <i class="nav-icon bi bi-circle"></i>
+                                    <p>Slider</p>
+                                </a>
+                            </li>
+                        </ul> --}}
                     </li>
                 @endcan
 
